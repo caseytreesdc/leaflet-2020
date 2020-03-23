93f9b5f97e0477835ea53bba98839fa7d7d5c723
Tissa Khosla <tissakhosla@Tissas-MacBook-Pro.local>
2020-03-23 11:41:30 -0400

    starting script file for replacement  - new branch to "script"

--- link-insert.py
+++ link-insert.py
@@ -1,1 +1,11 @@
-leaflet = open("create-a-file.html", "x")
+import datetime
+
+today = datetime.datetime.now()
+
+filename_prefix = today.strftime("%m-%d-%Y")
+
+new_leaflet_filename = f"{filename_prefix}-leaflet.html"
+
+new_links = open("link-list.html", "r")
+
+template = open("leaflet.html", "r")
